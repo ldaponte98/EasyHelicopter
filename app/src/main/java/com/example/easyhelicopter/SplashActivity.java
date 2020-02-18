cf7509e0cdb169cb0470596df4219614dd6ee6a1
Alvaro Enrique Rincones Alfaro <soportetecnico.codewind@gmail.com>
2020-02-18 15:53:49 -0500

    primer

--- app/src/main/java/com/example/easyhelicopter/SplashActivity.java
+++ app/src/main/java/com/example/easyhelicopter/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                
+
             }
         }, 2000);
     }
